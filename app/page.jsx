c74542880bc7022c1b9057c15eafd6deed446515
Nathan0769 <nathanrivollier69@gmail.com>
2025-06-27 11:56:46 +0200

    init project

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex lg:flex-col gap-8 min-h-full justify-center items-center ">
+      <div>Card</div>
+      <div>Preview</div>
+    </main>
+  );
+}
